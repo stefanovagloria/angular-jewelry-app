3d566b526de1e6dc44d1fe9e46679aec2a1f1e62
stefanovagloria <gloriqstefanova97@gmail.com>
2023-08-23 07:35:37 +0300

    Sample Create and Read queries added

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@angular/core';
-import {Firestore, collection, collectionData, addDoc, doc, deleteDoc} from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private fs: Firestore) {}
 
-  constructor(private fs: Firestore) { }
+  addProduct(prod: object) {
+    let currentProduct = { product: prod };
+    let productsCollection = collection(this.fs, 'products');
+     addDoc(productsCollection, currentProduct).then(()=> {
+      console.log('Created product!')
+     }).catch((err) => {
+      console.log(err);
+     })
+  }
 
-  getProducts(){
+  getProducts() {
     let productsCollection = collection(this.fs, 'products');
     return collectionData(productsCollection);
   }
 
-
-  addProduct(prod: object){
-    let currentProduct = {product: prod};
-    let productsCollection = collection(this.fs, 'products');
-    return addDoc(productsCollection,currentProduct);
+  updateProduct(
+    productId: string,
+    productName: string,
+    price: number,
+    category: string
+  ) {
+    const docRef = doc(this.fs, 'products', productId);
+    updateDoc(docRef, { productName, price, category });
+    return;
   }
 
-  deleteProduct(id: string){
+  deleteProduct(id: string) {
     let docRef = doc(this.fs, `products/${id}`);
     return deleteDoc(docRef);
   }
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../interfaces/product';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit{
+  products: Product[] = [];
 
-  products: Product[] = [
-    {
-      productName: 'Bracelet',
-      price: 45,
-      category: 'Silver'
-    },
-    {
-      productName: 'Earrings',
-      price: 50,
-      category: 'Gold'
-    },
-  ]
- 
+  constructor(private apiService: ApiService){
+    this.getData();
+  }
+
+  ngOnInit(): void {
+    this.apiService.getProducts();
+  }
+
+  addData(){
+    this.apiService.addProduct({productName: 'Earrings2', price: 30, category: 'Gold'});
+  }
+
+  getData(){
+    this.apiService.getProducts().subscribe((value) => {
+      console.log(value)
+    })
+  }
+
 }
