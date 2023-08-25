91286877e56f881e7856b72b6e4dd149dc4459e8
stefanovagloria <gloriqstefanova97@gmail.com>
2023-08-25 22:15:54 +0300

    Added addProduct query

--- src/app/product-editor/product-editor.component.ts
+++ src/app/product-editor/product-editor.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 
-import {FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-product-editor',
   templateUrl: './product-editor.component.html',
-  styleUrls: ['./product-editor.component.css']
+  styleUrls: ['./product-editor.component.css'],
 })
 export class ProductEditorComponent {
+  constructor(private apiService: ApiService) {}
 
   productForm = new FormGroup({
     productName: new FormControl(''),
     price: new FormControl(''),
     category: new FormControl(''),
-  })
+  });
+
+  onSubmit() {
+    this.apiService.addProduct({
+      productName: this.productForm.value.productName,
+      price: Number(this.productForm.value.price),
+      category: this.productForm.value.category,
+    });
+  }
 }
